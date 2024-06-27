8c8be8133f7df56381aa7ad6e6081bc958a15e71
adiliqbal95 <adil.iqbal@hotmail.co.uk>
2024-06-27 17:20:36 +0100

    implemented functionality to add new cake recipe

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       </header>
       <main>
         <RecipeListContainer />
-        <Search />
+        {/* <Search /> */}
       </main>
     </>
   );
--- src/CakeForm.jsx
+++ src/CakeForm.jsx
@@ -1,13 +1,27 @@
-const CakeForm = () => {
+import { useState } from "react";
+
+const CakeForm = ({cakes, setCakes}) => {
+
+    const [cakeName, setCakeName] = useState("")
+    const [ingredients, setIngredients] = useState([])
+    const [currentIngredient, setCurrentIngredient] = useState("");
+    const [rating, setRating] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // perform validation (TODO)
+        const newCake = {cakeName, ingredients, rating}
+        setCakes([newCake, ...cakes])
+    }
 
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label htmlFor="name">Cake Name:</label>
-            <input type="text" id="name" />
+            <input type="text" id="cake-name" name="cakeName" placeholder="Enter cake name..." value={cakeName} onChange={(e) => setCakeName(e.target.value)}/>
             <label htmlFor="ingredients">Ingredients:</label>
-            <textarea cols="30" rows="1" id="ingredients"></textarea>
+            <textarea cols="30" rows="1" id="ingredients" name="ingredients" placeholder="Enter ingredients..." value={ingredients} onChange={(e) => setIngredients(e.target.value.split(","))}></textarea>
             <label htmlFor="rating">Rating:</label>
-            <input type="number" id="rating" min={1} max={5}/>
+            <input type="number" id="rating" min={1} max={5} name="rating" placeholder="Enter rating..." value={rating} onChange={(e) => setRating(e.target.value)}/>
             <input type="submit"/>
         </form>
     )
